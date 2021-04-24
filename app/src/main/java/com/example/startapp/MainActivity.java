7dd798740547076282c88e939e3e40534c487097
timof <timofeykusch@ya.ru>
2021-04-24 13:37:03 +0300

    view instead surface

--- app/src/main/java/com/example/startapp/MainActivity.java
+++ app/src/main/java/com/example/startapp/MainActivity.java
@@ -2,27 +2,40 @@ package com.example.startapp;
 
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private Button startButton;
+    private MyDraw md;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((MySurfaceView) findViewById(R.id.mySurfaceView)).setMainActivity(this);
+        md = findViewById(R.id.myDraw);
+        md.setMa(this);
+
         startButton = findViewById(R.id.button);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //начать запись видео
-                startButton.setAlpha(0);
-                ((FrameLayout)findViewById(R.id.fl)).setAlpha(0);
+                startButton.setText("Идёт запись");
+                startButton.setEnabled(false);
+                md.setPause(false);
+
             }
         });
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        md.setPause(true);
+        startButton.setText("Запись не удалась. Начать заново.");
+        startButton.setEnabled(true);
+    }
+
 }
---
+++ app/src/main/java/com/example/startapp/bin/DrawThread.java
@@ -0,0 +1,14 @@
+package com.example.startapp.bin;
+
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+
+public class DrawThread extends Thread {
+    private boolean runFlag = false;
+    private SurfaceHolder surfaceHolder;
+
+}
